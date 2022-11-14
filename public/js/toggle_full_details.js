3519b79af54023f943d86dbff8b39f23fc1de1c4
peternatewood <peternatewood@tutanota.com>
2022-11-13 21:15:24 -0500

    Fix toggling materials cells with nested spans

--- public/js/toggle_full_details.js
+++ public/js/toggle_full_details.js
@@ -1,5 +1,12 @@
 ready(() => {
-	var MOUSE_MOVE_THRESHOLD = { x: 8, y: 8 };
+	function getMaterialsCell(element) {
+		if (element.classList.contains("materials")) {
+			return element;
+		}
+
+		return getMaterialsCell(element.parentElement);
+	}
+
 	var mousePos = { x: 0, y: 0 };
 	var toggleFullDetails = (cell, toggleOn) => {
 		if (toggleOn) {
@@ -12,17 +19,29 @@ ready(() => {
 		cell.dataset.full = toggleOn ? "1" : "0";
 	}
 	var handleMaterialsMousedown = (event) => {
-		mousePos.x = event.screenX;
-		mousePos.y = event.screenY;
+		// Only handle primary click (usually left mouse button)
+		if (event.button == 0) {
+			mousePos.x = event.screenX;
+			mousePos.y = event.screenY;
+		}
 	}
 	var handleMaterialsMouseup = (event) => {
-		if (Math.abs(event.screenX - mousePos.x) < MOUSE_MOVE_THRESHOLD.x && Math.abs(event.screenY - mousePos.y) < MOUSE_MOVE_THRESHOLD.y) {
-			var materialsCell = event.target;
-			if (event.target.parentElement.classList.contains("materials")) {
-				var materialsCell = event.target.parentElement;
-			}
+		const MOUSE_MOVE_THRESHOLD = { x: 4, y: 4 };
 
-			toggleFullDetails(materialsCell, materialsCell.dataset.full == "0");
+		// Only handle primary click (usually left mouse button)
+		if (event.button == 0) {
+			var xDistance = Math.abs(event.screenX - mousePos.x);
+			var yDistance = Math.abs(event.screenY - mousePos.y);
+
+			if (xDistance < MOUSE_MOVE_THRESHOLD.x && yDistance < MOUSE_MOVE_THRESHOLD.y) {
+				var materialsCell = getMaterialsCell(event.target);
+
+				if (!materialsCell.dataset.full) {
+					materialsCell.dataset.full = "0";
+				}
+
+				toggleFullDetails(materialsCell, materialsCell.dataset.full == "0");
+			}
 		}
 	}
 
