4cc331c53fa956269c8576eca080629afa727bed
peternatewood <peternatewood@tutanota.com>
2023-02-08 10:50:38 -0500

    Add all items and materials to items page

--- public/js/toggle_full_details.js
+++ public/js/toggle_full_details.js
@@ -1,10 +1,10 @@
 ready(() => {
-	function getMaterialsCell(element) {
-		if (element.classList.contains("materials")) {
+	function getExpandableCell(element) {
+		if (element.classList.contains("expandable")) {
 			return element;
 		}
 
-		return getMaterialsCell(element.parentElement);
+		return getExpandableCell(element.parentElement);
 	}
 
 	var mousePos = { x: 0, y: 0 };
@@ -18,14 +18,14 @@ ready(() => {
 
 		cell.dataset.full = toggleOn ? "1" : "0";
 	}
-	var handleMaterialsMousedown = (event) => {
+	var handleExpandableMousedown = (event) => {
 		// Only handle primary click (usually left mouse button)
 		if (event.button == 0) {
 			mousePos.x = event.screenX;
 			mousePos.y = event.screenY;
 		}
 	}
-	var handleMaterialsMouseup = (event) => {
+	var handleExpandableMouseup = (event) => {
 		const MOUSE_MOVE_THRESHOLD = { x: 4, y: 4 };
 
 		// Only handle primary click (usually left mouse button)
@@ -34,13 +34,13 @@ ready(() => {
 			var yDistance = Math.abs(event.screenY - mousePos.y);
 
 			if (xDistance < MOUSE_MOVE_THRESHOLD.x && yDistance < MOUSE_MOVE_THRESHOLD.y) {
-				var materialsCell = getMaterialsCell(event.target);
+				var expandableCell = getExpandableCell(event.target);
 
-				if (!materialsCell.dataset.full) {
-					materialsCell.dataset.full = "0";
+				if (!expandableCell.dataset.full) {
+					expandableCell.dataset.full = "0";
 				}
 
-				toggleFullDetails(materialsCell, materialsCell.dataset.full == "0");
+				toggleFullDetails(expandableCell, expandableCell.dataset.full == "0");
 			}
 		}
 	}
@@ -48,28 +48,27 @@ ready(() => {
 	var toggleFullCells = document.getElementById("cells-toggle");
 	var toggleOn = false;
 	if (toggleFullCells) {
-		if (window.localStorage.getItem("cellState")) {
-			toggleOn = true;
-			toggleFullCells.checked = true;
-		}
+		var cellState = window.localStorage.getItem("cellState");
+		toggleOn = cellState == "1";
+		toggleFullCells.checked = toggleOn;
 
 		toggleFullCells.addEventListener("change", event => {
 			toggleOn = event.target.checked;
-			for (let i = 0; i < materialCells.length; i++) {
-				toggleFullDetails(materialCells[i], toggleOn);
+			for (let i = 0; i < expandableCells.length; i++) {
+				toggleFullDetails(expandableCells[i], toggleOn);
 			}
 
 			window.localStorage.setItem("cellState", toggleOn ? "1" : "0");
 		});
 	}
 
-	var materialCells = document.getElementsByClassName("materials");
-	for (var i = 0; i < materialCells.length; i++) {
-		materialCells[i].addEventListener("mousedown", handleMaterialsMousedown);
-		materialCells[i].addEventListener("mouseup", handleMaterialsMouseup);
+	var expandableCells = document.getElementsByClassName("expandable");
+	for (var i = 0; i < expandableCells.length; i++) {
+		expandableCells[i].addEventListener("mousedown", handleExpandableMousedown);
+		expandableCells[i].addEventListener("mouseup", handleExpandableMouseup);
 
 		if (toggleOn) {
-			toggleFullDetails(materialCells[i], true);
+			toggleFullDetails(expandableCells[i], true);
 		}
 	}
 });
